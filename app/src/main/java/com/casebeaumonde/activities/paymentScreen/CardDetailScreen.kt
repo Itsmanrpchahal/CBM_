c2787a50a05f821070ee313eeb53a687b796e7d1
Itsmanprchahal <chahalmanpreet0305@gmail.com>
2020-10-15 11:28:54 +0530

    Update 15 Oct 2020, 11:28

--- app/src/main/java/com/casebeaumonde/activities/paymentScreen/CardDetailScreen.kt
+++ app/src/main/java/com/casebeaumonde/activities/paymentScreen/CardDetailScreen.kt
@@ -1,28 +1,40 @@
 package com.casebeaumonde.activities.paymentScreen
 
 import android.app.DatePickerDialog
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.casebeaumonde.R
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.Stripe
+import com.stripe.android.TokenCallback
+import com.stripe.android.model.Card
+import com.stripe.android.model.Token
 import java.text.SimpleDateFormat
 import java.util.*
+import javax.security.auth.callback.Callback
 
 class CardDetailScreen : AppCompatActivity() {
 
-    private lateinit var plan_carddate : EditText
-    private lateinit var plan_name : TextView
-    private lateinit var plan_price : TextView
-    private lateinit var plan_cardholdername : EditText
-    private lateinit var plan_cardnumber : EditText
-    private lateinit var plan_cvc : EditText
-    private lateinit var plan_billingzipcode : EditText
-    private lateinit var plan_verfybt : Button
-    private lateinit var plan_cancel : Button
-    private lateinit var back_carddetail : ImageButton
+    private lateinit var plan_carddate: EditText
+    private lateinit var plan_name: TextView
+    private lateinit var plan_price: TextView
+    private lateinit var plan_cardholdername: EditText
+    private lateinit var plan_cardnumber: EditText
+    private lateinit var plan_cvc: EditText
+    private lateinit var plan_billingzipcode: EditText
+    private lateinit var plan_verfybt: Button
+    private lateinit var plan_cancel: Button
+    private lateinit var back_carddetail: ImageButton
+    private val PUBLISHABLE_KEY = "pk_test_OTNta0F2CKTUpYDDM7igKdml"
+    var token: Token? = null
     val c = Calendar.getInstance()
+    lateinit var stripe: Stripe
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +51,10 @@ class CardDetailScreen : AppCompatActivity() {
         val day = c.get(Calendar.DAY_OF_MONTH)
         // create an OnDateSetListener
         val dateSetListener = object : DatePickerDialog.OnDateSetListener {
-            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
-                                   dayOfMonth: Int) {
+            override fun onDateSet(
+                view: DatePicker, year: Int, monthOfYear: Int,
+                dayOfMonth: Int
+            ) {
                 c.set(Calendar.YEAR, year)
                 c.set(Calendar.MONTH, monthOfYear)
                 c.set(Calendar.DAY_OF_MONTH, dayOfMonth)
@@ -52,17 +66,37 @@ class CardDetailScreen : AppCompatActivity() {
         // when you click on the button, show DatePickerDialog that is set with OnDateSetListener
         plan_carddate.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View) {
-                DatePickerDialog(this@CardDetailScreen,R.style.DialogTheme,
+                DatePickerDialog(
+                    this@CardDetailScreen, R.style.DialogTheme,
                     dateSetListener,
                     // set DatePickerDialog to point to today's date when it loads up
                     c.get(Calendar.YEAR),
                     c.get(Calendar.MONTH),
-                    c.get(Calendar.DAY_OF_MONTH)).show()
+                    c.get(Calendar.DAY_OF_MONTH)
+                ).show()
             }
         })
 
         back_carddetail.setOnClickListener {
             onBackPressed()
+        }
+
+        plan_verfybt.setOnClickListener {
+            //val card = CardUtils.isValidCardNumber("4242424242424242")
+            val card2 = Card("4242424242424242", 10, 2020, "122")
+            val stripe1 = Stripe(this,"pk_test_OTNta0F2CKTUpYDDM7igKdml")
+            stripe1.createToken(card2, object : TokenCallback {
+                override fun onSuccess(token: Token?) {
+                    Toast.makeText(this@CardDetailScreen, "" + token, Toast.LENGTH_SHORT).show()
+                    Log.d("token",""+token)
+                    Log.d("card",""+card2)
+                }
+
+                override fun onError(error: Exception?) {
+                    Toast.makeText(this@CardDetailScreen, "Error" + error, Toast.LENGTH_SHORT).show()
+                }
+            })
+
         }
     }
 
@@ -80,7 +114,29 @@ class CardDetailScreen : AppCompatActivity() {
         plan_cardnumber = findViewById(R.id.plan_cardnumber)
         plan_billingzipcode = findViewById(R.id.plan_billingzipcode)
         plan_verfybt = findViewById(R.id.plan_verfybt)
-        plan_cancel= findViewById(R.id.plan_cancel)
+        plan_cancel = findViewById(R.id.plan_cancel)
         back_carddetail = findViewById(R.id.back_carddetail)
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            // REQUEST_CODE_LOCATION should be defined on your app level
+            ActivityCompat.requestPermissions(this, permissions, 101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 101 && grantResults.isNotEmpty()
+            && grantResults[0] != PackageManager.PERMISSION_GRANTED
+        ) {
+            throw RuntimeException("Location services are required in order to " + "connect to a reader.")
+        }
     }
 }
---
+++ app/src/main/java/com/casebeaumonde/dialog/ErrorDialogFragment.java
@@ -0,0 +1,37 @@
+package com.casebeaumonde.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class ErrorDialogFragment extends DialogFragment {
+    public static ErrorDialogFragment newInstance(int titleId, String message) {
+        ErrorDialogFragment fragment = new ErrorDialogFragment();
+
+        Bundle args = new Bundle();
+        args.putInt("titleId", titleId);
+        args.putString("message", message);
+
+        fragment.setArguments(args);
+
+        return fragment;
+   }
+
+    public ErrorDialogFragment() {
+        // Empty constructor required for DialogFragment
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        int titleId = getArguments().getInt("titleId");
+        String message = getArguments().getString("message");
+
+        return new AlertDialog.Builder(getActivity())
+            .setTitle(titleId)
+            .setMessage(message)
+            .setPositiveButton("OK", (dialogInterface, i) -> dialogInterface.dismiss())
+            .create();
+    }
+}

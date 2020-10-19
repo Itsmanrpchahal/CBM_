b6326ff61f2393f6079ee382bee2001470c5ee08
Itsmanprchahal <chahalmanpreet0305@gmail.com>
2020-10-19 17:05:00 +0530

    Update 18 Oct 2020, 17:03

---
+++ app/src/main/java/com/casebeaumonde/fragments/profile/profileResponse/FollowUnFollowResponse.java
@@ -0,0 +1,32 @@
+package com.casebeaumonde.fragments.profile.profileResponse;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class FollowUnFollowResponse {
+
+    @SerializedName("code")
+    @Expose
+    private String code;
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}

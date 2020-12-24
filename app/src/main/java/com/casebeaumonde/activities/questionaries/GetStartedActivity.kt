26d3b40e5a7d7e91f62ff5d1ff271fe56b445e64
Itsmanprchahal <chahalmanpreet0305@gmail.com>
2020-12-24 17:16:53 +0530

    Update 24 Dec 2020 17:16

---
+++ app/src/main/java/com/casebeaumonde/activities/questionaries/GetStartedActivity.kt
@@ -0,0 +1,12 @@
+package com.casebeaumonde.activities.questionaries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.casebeaumonde.R
+
+class GetStartedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+    }
+}
---
+++ app/src/main/java/com/casebeaumonde/activities/questionaries/TellAboutYourSelf.kt
@@ -0,0 +1,12 @@
+package com.casebeaumonde.activities.questionaries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.casebeaumonde.R
+
+class TellAboutYourSelf : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tell_about_your_self)
+    }
+}

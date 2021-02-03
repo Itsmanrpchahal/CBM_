dd4140fbc9fb621ba461bd500a776f707380cdde
Itsmanprchahal <chahalmanpreet0305@gmail.com>
2021-02-03 13:21:38 +0530

    Update 3 feb,2021 13:21

--- app/src/main/java/com/casebeaumonde/fragments/profile/ViewProfile.kt
+++ app/src/main/java/com/casebeaumonde/fragments/profile/ViewProfile.kt
@@ -28,29 +28,30 @@ import kotlinx.android.synthetic.main.activity_view_profile.*
 import kotlinx.android.synthetic.main.fragment_my_wall.*
 import retrofit2.Response
 
-class ViewProfile : BaseClass(), Controller.UserProfileAPI,Controller.FollowUnFollowAPI,TabLayout.OnTabSelectedListener {
+class ViewProfile : BaseClass(), Controller.UserProfileAPI, Controller.FollowUnFollowAPI {
 
     private lateinit var controller: Controller
     private lateinit var utility: Utility
     private lateinit var pd: ProgressDialog
     private lateinit var userID: String
-    private lateinit var viewprofile_back : ImageButton
-    private lateinit var viewprofile__username : TextView
-    private lateinit var viewprofile_usernamefull : TextView
-    private lateinit var viewprofile_followerscount : TextView
-    private lateinit var viewprofile__followingcount : TextView
-    private lateinit var viewprofile_tabLayout : TabLayout
-    private lateinit var viewprofile_profilePic : ImageView
-    private lateinit var viewprofile__mygigs : Button
-    private lateinit var viewprofile_followbt : Button
-    private lateinit var role : String
+    private lateinit var viewprofile_back: ImageButton
+    private lateinit var viewprofile__username: TextView
+    private lateinit var viewprofile_usernamefull: TextView
+    private lateinit var viewprofile_followerscount: TextView
+    private lateinit var viewprofile__followingcount: TextView
+    private lateinit var viewprofile_profilePic: ImageView
+    private lateinit var viewprofile__mygigs: Button
+    private lateinit var viewprofile_followbt: Button
+    private lateinit var viewprofile__mywall: Button
+    private lateinit var viewprofile__mycloset: Button
+    private lateinit var role: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_profile)
         userID = intent.getStringExtra("userID").toString()
         controller = Controller()
-        controller.Controller(this,this)
+        controller.Controller(this, this)
         findIds()
         controller.setUserProfileAPI(
             "Bearer " + getStringVal(Constants.TOKEN),
@@ -65,14 +66,24 @@ class ViewProfile : BaseClass(), Controller.UserProfileAPI,Controller.FollowUnFo
             onBackPressed()
         }
 
-        viewprofile__mygigs.setOnClickListener {
-            startActivity(Intent(this, MyGigs::class.java).putExtra("role", role).putExtra("userID",userID))
-        }
-
         viewprofile_followbt.setOnClickListener {
             pd.show()
             pd.setContentView(R.layout.loading)
-            controller.FollowUnFollow("Bearer "+getStringVal(Constants.TOKEN),userID)
+            controller.FollowUnFollow("Bearer " + getStringVal(Constants.TOKEN), userID)
+        }
+
+        viewprofile__mygigs.setOnClickListener {
+            startActivity(
+                Intent(this, MyGigs::class.java).putExtra("role", role).putExtra("userID", userID)
+            )
+        }
+
+        viewprofile__mywall.setOnClickListener {
+            startActivity(Intent(this, MyWall::class.java).putExtra("userID", userID))
+        }
+
+        viewprofile__mycloset.setOnClickListener {
+            startActivity(Intent(this, MyClosets::class.java).putExtra("userID", userID))
         }
     }
 
@@ -89,38 +100,42 @@ class ViewProfile : BaseClass(), Controller.UserProfileAPI,Controller.FollowUnFo
         viewprofile__username = findViewById(R.id.viewprofile__username)
         viewprofile_profilePic = findViewById(R.id.viewprofile_profilePic)
         viewprofile_usernamefull = findViewById(R.id.viewprofile_usernamefull)
-        viewprofile_followerscount=  findViewById(R.id.viewprofile_followerscount)
+        viewprofile_followerscount = findViewById(R.id.viewprofile_followerscount)
         viewprofile__followingcount = findViewById(R.id.viewprofile__followingcount)
-        viewprofile_tabLayout = findViewById(R.id.viewprofile_tabLayout)
         viewprofile__mygigs = findViewById(R.id.viewprofile__mygigs)
         viewprofile_followbt = findViewById(R.id.viewprofile_followbt)
+        viewprofile__mywall = findViewById(R.id.viewprofile__mywall)
+        viewprofile__mycloset = findViewById(R.id.viewprofile__mycloset)
     }
 
     override fun onPrfileSucess(userProfileResponse: Response<UserProfileResponse>) {
         pd.dismiss()
-        if (userProfileResponse.isSuccessful)
-        {
-            if (userProfileResponse.body()?.getData()?.currentFollowing==true)
-            {
+        if (userProfileResponse.isSuccessful) {
+            if (userProfileResponse.body()?.getData()?.currentFollowing == true) {
                 viewprofile_followbt.setText("UnFollow")
-            }else{
+            } else {
                 viewprofile_followbt.setText("Follow")
             }
-            viewprofile__username.setText(userProfileResponse.body()?.getData()?.user?.firstname+"'s Profile")
-            viewprofile_usernamefull.setText(userProfileResponse.body()?.getData()?.user?.firstname+" "+userProfileResponse.body()?.getData()?.user?.lastname)
-            viewprofile_followerscount.text = userProfileResponse.body()?.getData()?.user?.followers?.size.toString()
-            viewprofile__followingcount.text = userProfileResponse.body()?.getData()?.user?.following?.size.toString()
-            Glide.with(this).load(Constants.BASE_IMAGE_URL+userProfileResponse.body()?.getData()?.user?.avatar).placeholder(R.drawable.login_banner).into(viewprofile_profilePic)
+            viewprofile__username.setText(
+                userProfileResponse.body()?.getData()?.user?.firstname + "'s Profile"
+            )
+            viewprofile_usernamefull.setText(
+                userProfileResponse.body()
+                    ?.getData()?.user?.firstname + " " + userProfileResponse.body()
+                    ?.getData()?.user?.lastname
+            )
+            viewprofile_followerscount.text =
+                userProfileResponse.body()?.getData()?.user?.followers?.size.toString()
+            viewprofile__followingcount.text =
+                userProfileResponse.body()?.getData()?.user?.following?.size.toString()
+            Glide.with(this).load(
+                Constants.BASE_IMAGE_URL + userProfileResponse.body()?.getData()?.user?.avatar
+            ).placeholder(R.drawable.login_banner).into(viewprofile_profilePic)
 
             role = userProfileResponse.body()?.getData()?.user?.role.toString()
 
-                viewprofile_tabLayout!!.addTab(viewprofile_tabLayout!!.newTab().setText("My Wall"))
-                viewprofile_tabLayout!!.addTab(viewprofile_tabLayout!!.newTab().setText("My services"))
-                viewprofile_tabLayout!!.addTab(viewprofile_tabLayout!!.newTab().setText("My Closets"))
-                viewprofile_tabLayout!!.tabGravity = TabLayout.GRAVITY_FILL
-                viewprofile_tabLayout!!.setOnTabSelectedListener(this)
 
-        }else{
+        } else {
             utility!!.relative_snackbar(
                 parent_viewProfile,
                 userProfileResponse.message(),
@@ -132,12 +147,10 @@ class ViewProfile : BaseClass(), Controller.UserProfileAPI,Controller.FollowUnFo
 
     override fun onFollowUnfollowSuccess(followUnfollow: Response<FollowUnFollowResponse>) {
         pd.dismiss()
-        if (followUnfollow.isSuccessful)
-        {
-            if (followUnfollow.body()?.getMessage().equals("Now you are following Roberto"))
-            {
+        if (followUnfollow.isSuccessful) {
+            if (followUnfollow.body()?.getMessage().equals("Now you are following Roberto")) {
                 viewprofile_followbt.setText("Unfollow")
-            }else{
+            } else {
                 viewprofile_followbt.setText("Follow")
             }
             controller.setUserProfileAPI(
@@ -167,43 +180,6 @@ class ViewProfile : BaseClass(), Controller.UserProfileAPI,Controller.FollowUnFo
             error,
             getString(R.string.close_up)
         )
-        Log.d("message",""+error)
-    }
-
-    override fun onTabSelected(p0: TabLayout.Tab?) {
-        when {
-            p0?.text?.equals("My Closets")!! -> {
-                startActivity(Intent(this, MyClosets::class.java).putExtra("userID", userID))
-            }
-
-            p0?.text?.equals("My Wall")!! -> {
-                startActivity(Intent(this, MyWall::class.java).putExtra("userID", userID))
-            }
-
-            p0?.text?.equals("My gigs")!! -> {
-                startActivity(Intent(this, MyGigs::class.java).putExtra("role", role).putExtra("userID",userID))
-            }
-
-        }
-    }
-
-    override fun onTabUnselected(p0: TabLayout.Tab?) {
-
-    }
-
-    override fun onTabReselected(p0: TabLayout.Tab?) {
-        when {
-            p0?.text?.equals("My Closets")!! -> {
-                startActivity(Intent(this, MyClosets::class.java).putExtra("userID", userID))
-            }
-
-            p0?.text?.equals("My Wall")!! -> {
-                startActivity(Intent(this, MyWall::class.java).putExtra("userID", userID))
-            }
-
-            p0?.text?.equals("My gigs")!! -> {
-                startActivity(Intent(this, MyGigs::class.java).putExtra("role", role).putExtra("userID",userID))
-            }
-        }
+        Log.d("message", "" + error)
     }
 }
